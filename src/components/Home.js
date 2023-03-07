f2fa5e6815fd881a39b8ff6bf0c13c9d8f1c5716
Kickstartlab <info@kickstartlabs.io>
2023-03-07 15:14:22 +0500

    add counter

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,6 +16,12 @@ import side from '../assets/side.png'
 
 
 import Footer from './Footer'
+import CountdownTimer from './CountDownTimer'
+
+const SEVEN_DAYS_IN_MS = 7 * 24 * 60 * 60 * 1000;
+const NOW_IN_MS = new Date().getTime();
+
+const dateTimeAfterSevenDays = NOW_IN_MS + SEVEN_DAYS_IN_MS;
 
 
 function Home() {
@@ -146,6 +152,8 @@ function Home() {
                                     <p className='font-semibold text-blue-50'>
                                         Live Launch date
                                     </p>
+
+                                    <CountdownTimer targetDate={dateTimeAfterSevenDays} />
 
                                 </div>
 
